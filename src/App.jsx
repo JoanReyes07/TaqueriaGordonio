fbb0df7642cd3c06ebfc7ca04d79879258f79ad1
JoanReyes07 <rjoan310@gmail.com>
2024-11-09 16:20:14 -0600

    Cambio slick

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import PaginaInicio from "./components/PaginaInicio/PaginaInicio";
 import PaginaMenu from "./components/PaginaMenu/PaginaMenu";
 import PaginaProductosTortas from "./components/PaginProductos/PaginaProductosTortas";
 import PaginaProductosTacos from "./components/PaginProductos/PaginaProductosTacos";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
 
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<PaginaInicio />} />
-      <Route path="/Menu-General" element={<PaginaMenu />} />
-      <Route path="/Menu-Platillo-Tacos" element={<PaginaProductosTacos />} />
-      <Route path="/Menu-Platillo-Tortas" element={<PaginaProductosTortas />} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaInicio />} />
+        <Route path="/Menu-General" element={<PaginaMenu />} />
+        <Route path="/Menu-Platillo-Tacos" element={<PaginaProductosTacos />} />
+        <Route path="/Menu-Platillo-Tortas" element={<PaginaProductosTortas />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
